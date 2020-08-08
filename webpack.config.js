5425116a5556694218e10e91a5d95c2749dd3cf1
Joerg Beekmann <joerg@purepaces.com>
2020-08-08 11:56:33 -0700

    update to using wrangler

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,22 @@
 const CloudflareWorkerPlugin = require('cloudflare-workers-webpack-plugin');
 
 module.exports = env => {
-    var env = env
-        ? {deploy: true, CF_EMAIL: env.CF_EMAIL, CF_KEY: env.CF_KEY}
-        : {prod: false, CF_EMAIL: "", CF_KEY: ""}
+    // var env = env
+    //     ? {deploy: true, CF_EMAIL: env.CF_EMAIL, CF_KEY: env.CF_KEY}
+    //     : {prod: false, CF_EMAIL: "", CF_KEY: ""}
 
     return {
         entry: {worker: ['./src/Worker.fsproj']},
         target: 'webworker',
         output: {
-            path: __dirname,
-            filename: '[name].bndl.js',
+            path: __dirname + '/worker',
         },
         mode: "production",
         resolve: {
             // See https://github.com/fable-compiler/Fable/issues/1490
             symlinks: false
         },
-        plugins: [
-            new CloudflareWorkerPlugin(
-                // CF credentials for instructions on where to find them see:
-                // https://developers.cloudflare.com/workers/api/
-                env.CF_EMAIL,
-                env.CF_KEY,
-                {   // Options object, for addtional options see:
-                    // https://www.npmjs.com/package/cloudflare-worker-webpack-plugin
-                    site: `rec-room.io`,
-                    enabledPatterns: `rec-room.io/*`,
-                    clearRoutes: true,
-                    verbose: true,
-                    colors: true,
-                    enabled: env.prod,
-                    script: "worker.bndl.js"
-                } 
-            )
-        ],
+        plugins: [ ],
         module: {
             rules: [
                 // - fable-loader: transforms F# into JS
